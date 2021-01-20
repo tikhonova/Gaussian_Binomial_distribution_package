4c3dd5f1a4ded262523afbad3091afcd4b0f5e08
Tatiana Tikhonova <tatianatikhonova@Tatianas-MacBook-Air.local>
2021-01-20 17:21:38 -0500

    Upload package files

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Oh lala, it's time for SETUP, baby."""
+
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='Gaussian_distribution_pkg',
+    version='0.1',
+    description='Gaussian distributions',
+    packages=['Gaussian_distribution_pkg'],
+    author="Tatoshka",
+    zip_safe=False,
+    url="https://github.com/pypa/sampleproject",
+    long_description=open('README.txt').read()
+)
