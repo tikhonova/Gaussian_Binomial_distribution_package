500cbff57143970bba4ddaab9b938867957bf888
Tatiana Tikhonova <tatianatikhonova@Tatianas-MacBook-Air.local>
2021-01-21 16:58:21 -0500

    Update setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-"""Oh lala, it's time for SETUP, baby."""
+"""Oh lala, it's time for the SETUP, baby."""
 
 import setuptools
 from distutils.core import setup
 
 setup(
-    name='Gaussian_distribution_pkg',
+    name='Gaussian_and_Binomial_distribution_pkg',
     version='0.1',
     description='Gaussian distributions',
-    packages=['Gaussian_distribution_pkg'],
-    author="Tatoshka",
+    packages=['Gaussian_and_Binomial_distribution_pkg'],
+    author="Tatiana",
     zip_safe=False,
     url="https://github.com/pypa/sampleproject",
     long_description=open('README.txt').read()
